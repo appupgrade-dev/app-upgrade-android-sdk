71a3c396e412b2fdaa1b6f9b10ffd3d4f89172fb
App Upgrade <appupgrade.dev@gmail.com>
2023-02-28 03:27:36 +0530

    Add optional preferredAndroidMarket

--- app-upgrade-android-sdk/src/main/java/com/appupgrade/app_upgrade_android_sdk/models/AppInfo.kt
+++ app-upgrade-android-sdk/src/main/java/com/appupgrade/app_upgrade_android_sdk/models/AppInfo.kt
@@ -9,4 +9,31 @@ data class AppInfo(
     val appLanguage: String? = null,
     val preferredAndroidMarket: String? = null,
     val otherAndroidMarketUrl: String? = null,
-)
+) {
+    constructor(
+        appId: String,
+        appName: String,
+        appVersion: String,
+        platform: String,
+        environment: String,
+    ): this(appId, appName, appVersion, platform, environment, null, null, null)
+
+    constructor(
+        appId: String,
+        appName: String,
+        appVersion: String,
+        platform: String,
+        environment: String,
+        appLanguage: String,
+    ): this(appId, appName, appVersion, platform, environment, appLanguage, null, null)
+
+    constructor(
+        appId: String,
+        appName: String,
+        appVersion: String,
+        platform: String,
+        environment: String,
+        appLanguage: String,
+        preferredAndroidMarket: String?,
+    ): this(appId, appName, appVersion, platform, environment, appLanguage, preferredAndroidMarket, null)
+}
